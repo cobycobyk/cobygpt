1ff73a8bf35dfa7133c382d21ce57a8136423066
Coby Kabili <ckabili8@gmail.com>
2023-03-22 09:01:11 -0600

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import {BoltIcon, ExclamationTriangleIcon, SunIcon} from '@heroicons/react/24/ou
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center h-screen px-2 text-white'>
-      <h1 className='text-5xl font-bold mb-20'>Chat GPT</h1>
+      <h1 className='text-5xl font-bold mb-3'>Coby GPT</h1>
+      <h5 className='text-xl font-bold mb-20 text-gray-400'>ChatGPT Clone</h5>
       <div className='flex space-x-2 text-center'>
         <div>
           <div className='flex flex-col items-center justify-center mb-5'>
